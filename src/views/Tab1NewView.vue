351cce222b225c4739286375b66e0e1bfbc50751
Maria <maria-verena@martin-schubert.com>
2023-07-19 15:03:01 +0200

    tab1 improved

---
+++ src/views/Tab1NewView.vue
@@ -0,0 +1,36 @@
+<template>
+
+Tab 2
+<div id="lila-balken2">
+  <p></p>
+  <h2> hi</h2> ("")
+
+  <div class="burger-menu centered">
+    <div  class="burger-icon">&#9776;</div>
+
+    <div> id= profile picture</div>
+  </div>
+</div>
+
+<h2> Here are your most heard: </h2>
+<p></p>
+<p></p>
+<h4>songs</h4>
+<h4>albums</h4>
+<h4>generes</h4>
+
+Tab 3 
+<p></p>
+<h4>analyzing</h4>
+<div class="burger-menu centered"></div>
+ <div  class="burger-icon">&#9776;</div>
+<div id="lila-balken3">
+  <p></p>
+  <h3> with </h3>
+</div>
+<h4>songs</h4>
+<h4>albums</h4>
+<h4>generes</h4>
+
+
+</template>
--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -6,20 +6,102 @@
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
-        </ion-toolbar>
-      </ion-header>
 
-      <div id="spotify-buttons">
-        <ion-button @click="connect()">Mit Spotify verbinden</ion-button>
-        <ion-button @click="disconnect()">Spotify Verbindung lösen</ion-button>
-        <ion-button @click="test()">Test</ion-button>
+      <div id="lila-balken">
+        <div class="burger-menu centered">
+          <div  class="burger-icon">&#9776;</div>
+        </div>
+      </div>
+
+      <div id="ellipse-menu">
+        <p></p>
+        <button><h5>Login</h5></button>
+        <p></p>
+        <button><h5>Analyze my spottify</h5></button>
+        <p></p>
+        <button><h5>Compare my spotify</h5></button>
+      </div>
+      <h1>Learn more about your music taste and compare it with your friends (or strangers)! </h1>
+
+
+      <div id="login-ellipse" class="centered" >  
+          <button> <h5>Login</h5> </button>
+
       </div>
     </ion-content>
   </ion-page>
 </template>
+
+<style>
+
+#lila-balken {
+
+  height: 47px;
+  left: 0px;
+  top: 40px;
+
+  background: #7546B1;
+}
+
+#spotify-buttons {
+  background-color:rgba (117, 70, 177,0.70);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 5rem auto 0;
+}
+
+.burger-menu {
+
+  position: relative;
+  width: 42px;
+height: 42px;
+flex-shrink: 0;
+
+border-radius: 8px;
+background: #BD96E3;
+}
+
+.burger-icon {
+
+  cursor: pointer;
+  font-size: 1.5rem;
+}
+
+#ellipse-menu {
+  position: relative;
+  width: 427px;
+  height: 372px;
+
+  overflow: hidden;
+}
+
+#ellipse-menu::before {
+  display: block;
+  position: absolute;
+  content: "";
+  width: 427px;
+  height: 372px;
+  left: -223px;
+  border-radius:  0% 50% 50% 0  ;
+  overflow: hidden;
+  background: rgba(117, 70, 177, 0.70);
+
+}
+
+#login-ellipse {
+  width: 100px;
+  height: 100px;
+  background-color: #BD96E3;
+  border-radius: 50%;
+}
+
+.centered {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
 
 <script setup>
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/vue';
@@ -37,12 +119,3 @@ async function test() {
   await spotifySearch()
 }
 </script>
-
-<style>
-#spotify-buttons {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 5rem auto 0;
-}
-</style>
